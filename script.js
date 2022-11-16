7488928e6c06c73526e2a5caf7ee11130cf4862e
RizeKishimaro <jokerlove267@gmail.com>
2022-11-16 09:19:11 +0630

    Added Bootstrap

---
+++ script.js
@@ -0,0 +1,8 @@
+// let string = ['a','b','c','d'];
+// let result = string.reverse();
+// for(let i = 0;i<result.length;i++){
+//     document.writeln(result[i]);
+// }
+// for(let i=0;i<result.length;i--){
+//     document.writeln(result[i])
+// }
