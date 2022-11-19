696952a403d766f08c10d5815ab4c8c3165242db
RizeKishimaro <jokerlove267@gmail.com>
2022-11-19 22:23:41 +0630

    Added projects

---
+++ Pratice1/script.js
@@ -0,0 +1,6 @@
+// document.addEventListener("mousemove",(event)=>{
+//     console.log("Client X value:"+event.clientX);
+//     console.log("Client Y value:"+event.clientY);
+//     console.log("Client X value:"+event.clientX +"Y value :" +event.clientY);
+
+// })
